8553d5ab46408888ed7cf824552f9147ecf85391
Matemaatikko <juho_salmensuu@hotmail.com>
2022-08-12 23:57:07 +0300

    c

--- NodeLocationShuffler/Source/Shuffler_ConfigurationStruct.h
+++ NodeLocationShuffler/Source/Shuffler_ConfigurationStruct.h
@@ -2,22 +2,22 @@
 #include "CoreMinimal.h"
 #include "Configuration/ConfigManager.h"
 #include "Engine/Engine.h"
-#include "ShufflerConfigurationStruct.generated.h"
+#include "Shuffler_ConfigurationStruct.generated.h"
 
-/* Struct generated from Mod Configuration Asset '/OreDepositLocationRandomizer/ShufflerConfiguration' */
+/* Struct generated from Mod Configuration Asset '/NodeLocationShuffler/Shuffler_Configuration' */
 USTRUCT(BlueprintType)
-struct FShufflerConfigurationStruct {
+struct FShuffler_ConfigurationStruct {
     GENERATED_BODY()
 public:
     UPROPERTY(BlueprintReadWrite)
     int32 seed;
 
     /* Retrieves active configuration value and returns object of this struct containing it */
-    static FShufflerConfigurationStruct GetActiveConfig() {
-        FShufflerConfigurationStruct ConfigStruct{};
+    static FShuffler_ConfigurationStruct GetActiveConfig() {
+        FShuffler_ConfigurationStruct ConfigStruct{};
         FConfigId ConfigId{"OreLocationShufflerMod", ""};
         UConfigManager* ConfigManager = GEngine->GetEngineSubsystem<UConfigManager>();
-        ConfigManager->FillConfigurationStruct(ConfigId, FDynamicStructInfo{FShufflerConfigurationStruct::StaticStruct(), &ConfigStruct});
+        ConfigManager->FillConfigurationStruct(ConfigId, FDynamicStructInfo{FShuffler_ConfigurationStruct::StaticStruct(), &ConfigStruct});
         return ConfigStruct;
     }
 };
